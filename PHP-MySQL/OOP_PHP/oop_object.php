f57b44cc220743fc20a22321523640b226129964
shahlalhossain <shahlalhossain@gmail.com>
2021-11-24 22:01:56 +0600

    OOP Class Extend

--- PHP-MySQL/OOP_PHP/oop_object.php
+++ PHP-MySQL/OOP_PHP/oop_object.php
@@ -9,8 +9,26 @@ class Fruit {
     function set_name($name) {
         $this->name = $name;
     }
+    function set_color($color) {
+        $this->color = $color;
+    }
     function get_name() {
         return $this->name;
+    }
+    function get_color() {
+        return $this->color;
+    }
+    
+    function setNameAndColor($name, $color) {
+        $this->name = $name;
+        $this->color = $color;
+    }
+    
+    function getNameAndColor($name, $color) {
+        $data['name'] = $this->name;
+        $data['color'] = $this->color;
+        
+        return $data;
     }
 }
 
